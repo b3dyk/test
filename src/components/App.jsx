840c550ba9788f6125179915b74ad28aab7ef0dc
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-02-06 01:54:08 +0200

    basic functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import 'ag-grid-community/styles//ag-grid.css';
-import 'ag-grid-community/styles//ag-theme-alpine.css';
 import CSVReader from 'react-csv-reader';
 import { DataTable } from './DataTable/DataTable';
 import { addTransactions } from 'redux/transactions/transactionsSlice';
